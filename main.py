a22aa6fd4a17ea4de0f4ed43f0c07b1a79a7f985
dcejadelta <deana.ceja@deltacollege.edu>
2025-11-12 17:57:30 +0000

    finished the assignment

--- main.py
+++ main.py
@@ -1,1 +1,40 @@
+'''
+This is the first programming assignment showing user input
+Name: P.Ceja
+Date: 11/12/2025
+'''
+#this is a single line comment
+
+#name = input("What is your name?")
+
+#print("Hello, Pancho")
+
+'''Data Types
+----------------
+Classification for the different kinds of data
+available in your programming language'''
+
+#int - whole numbers
+#answer = 14 + 65
+#print(answer)
+#check_type = type(answer)
+#print(check_type)
+
+#float - decimal numbers
+#deci_ans = round(14/65,3)
+#print(deci_ans)
+#print(type(deci_ans))
+
+#string
+string1 = "Hiii"
+string2 = "25"
+string3 = "a"
+string4 = '''hi this is a super long string
+this is a really cool story i promise, only not really because
+im just typing'''
+
+print(string1)
+print(string2)
+print(string3)
+print(string4)
 
